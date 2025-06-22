71bd516ed74753fbc150bdc4b904199b9c1a8d61
mothtoaflame44 <listetriafusion@gmail.com>
2025-06-22 23:46:51 +0600

    conditionals

---
+++ script.js
@@ -0,0 +1,25 @@
+let Rahim;
+let Karim;
+let Rafiq;
+
+Rahim ="cycle";
+Karim = "car";
+Rafiq = "bike";
+
+if ( Rahim == "bike"){
+    console.log('I will go to market');
+
+}
+
+else if (Karim == "cycle"){
+    console.log('I will go to playground');
+}
+
+else if (Rafiq == "car"){
+    console.log('I will go to park');
+
+}
+
+else{
+    console.log('I will go to sleep');
+}
